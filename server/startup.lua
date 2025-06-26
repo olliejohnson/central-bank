91166b9488387f36ba542965a75e677fcf25c186
Oliver Johnson <oliver.joseph.johnson@gmail.com>
2025-06-25 23:02:46 -0400

    fixed check action

--- server/startup.lua
+++ server/startup.lua
@@ -3,8 +3,6 @@ local chatBox = peripheral.find("chat_box")
 
 chatBox.sendToastToPlayer("Test", "supa", "jacoberrol")
 
-chatBox.sendFormattedMessageToPlayer({ text = "Hello", bold = true, hover_event = { action = "show_text", value = { text = "Hover", color = "red", bold = true } } }, "jacoberrol", "", "")
-
 ---@class server
 local server = {}
 
